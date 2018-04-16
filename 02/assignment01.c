1b3b262f9ff305cf1f75667cf339976955b97fc8
ainaarita <csharpx2@gmail.com>
2018-04-16 16:53:44 +0900

    up

---
+++ 02/assignment01.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+  int t;
+  double r;
+  r=1.1;
+  double x, xx, k;
+  x=1.0;
+  k=300.0;
+
+   FILE *fp;
+   fp=fopen("outputr=1.1.dat", "w");
+   fprintf(fp, "%d, %f\n", 0, x);
+
+   for(t=1; t<300; t++){
+
+   xx=x+r*(1-x/k)*x;
+
+   fprintf(fp, "%d, %f\n", t, xx);
+   x=xx;
+ }
+ fclose(fp);
+
+ return 0;
+}
